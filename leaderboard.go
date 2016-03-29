3768b426dff9aa20e66febdb25f8365dad5f0ee2
Kamron Batman <kamron@gunghoonline.com>
2016-03-28 17:59:23 -0700

    [Add] Adds GAE support.

--- leaderboard.go
+++ leaderboard.go
@@ -3,16 +3,17 @@ package leaderboard
 import (
 	"fmt"
 	"math"
-	"time"
+	"net"
 
 	"github.com/garyburd/redigo/redis"
+	"appengine/socket"
 )
 
 /* Structs model */
 type User struct {
-	Name  string
-	Score int
-	Rank  int
+	PlayerID 	uint64
+	Score		int
+	Rank 		int
 }
 
 type Team struct {
@@ -24,15 +25,16 @@ type Team struct {
 type RedisSettings struct {
 	Host     string
 	Password string
+	// Added context so we have the currently valid AppEngine context.
+	Context appengine.Context
 }
 
 type Leaderboard struct {
+	AppEngine bool
 	Settings RedisSettings
 	Name     string
 	PageSize int
 }
-
-/* End Structs model */
 
 var pool *redis.Pool
 
@@ -63,10 +65,32 @@ func newPool(server string, password string) *redis.Pool {
 }
 
 func getConnection(settings RedisSettings) redis.Conn {
-	if pool == nil {
-		pool = newPool(settings.Host, settings.Password)
+
+	if settings.AppEngine {
+		// Since Google App Engine requires the use of socket.Dial (from appengine/socket), we have to override the default dialer.
+		// To add the custom dialer, we need to use redis.DialNetDial.
+		// redis.DialNetDial allows us to specify a custom dialer through the function signature func(string, string) (net.Conn, error).
+		c, err := redis.Dial("tcp", settings.Host, redis.DialNetDial(func(network, addr string) (net.Conn, error) {
+			return socket.Dial(settings.Context, network, addr) }))
+
+		if err != nil {
+			fmt.Printf("Redis connection error: %v", err)
+			return nil
+		}
+
+		if settings.Password != "" {
+			if _, err := c.Do("AUTH", settings.Password); err != nil {
+				c.Close()
+				fmt.Printf("Redis connection error: %v", err)
+				return nil
+			}
+		}
+
+		return c
+	} else if pool == nil {
+			pool = newPool(settings.Host, settings.Password)
+			return pool.Get()
 	}
-	return pool.Get()
 }
 
 func getMembersByRange(settings RedisSettings, leaderboard string, pageSize int, startOffset int, endOffset int) []User {
@@ -76,11 +100,15 @@ func getMembersByRange(settings RedisSettings, leaderboard string, pageSize int,
 	values, _ := redis.Values(conn.Do("ZREVRANGE", leaderboard, startOffset, endOffset, "WITHSCORES"))
 	var i = 0
 	for len(values) > 0 {
-		name := ""
+		playerid := uint64(0)
 		score := -1
-		values, _ = redis.Scan(values, &name, &score)
-		rank, _ := redis.Int(conn.Do("ZREVRANK", leaderboard, name))
-		nUser := User{Name: name, Score: score, Rank: rank + 1}
+		values, _ = redis.Scan(values, &playerid, &score)
+		rank, _ := redis.Int(conn.Do("ZREVRANK", leaderboard, playerid))
+		nUser := User{
+			PlayerID: playerid,
+			Score: score,
+			Rank: rank + 1,
+		}
 		users[i] = nUser
 		i += 1
 	}
@@ -92,76 +120,86 @@ func getMembersByRange(settings RedisSettings, leaderboard string, pageSize int,
 /* Public functions */
 
 func NewLeaderboard(settings RedisSettings, name string, pageSize int) Leaderboard {
-	l := Leaderboard{Settings: settings, Name: name, PageSize: pageSize}
+	l := Leaderboard{settings: settings, name: name, pageSize: pageSize}
 	return l
 }
 
-func (l *Leaderboard) RankMember(username string, score int) (User, error) {
+func (l *Leaderboard) RankMember(playerID uint64, score int) (User, error) {
 	conn := getConnection(l.Settings)
 	defer conn.Close()
-	_, err := conn.Do("ZADD", l.Name, score, username)
+	_, err := conn.Do("ZADD", l.Name, score, playerID)
 	if err != nil {
-		fmt.Printf("error on store in redis in rankMember Leaderboard:%s - Username:%s - Score:%d", l.Name, username, score)
+		fmt.Printf("error on store in redis in rankMember Leaderboard:%s - Username:%s - Score:%d", l.Name, playerID, score)
 	}
-	rank, err := redis.Int(conn.Do("ZREVRANK", l.Name, username))
+	rank, err := redis.Int(conn.Do("ZREVRANK", l.Name, playerID))
 	if err != nil {
-		fmt.Printf("error on get user rank Leaderboard:%s - Username:%s", l.Name, username)
+		fmt.Printf("error on get user rank Leaderboard:%s - Username:%s", l.Name, playerID)
 		rank = -1
 	}
-	nUser := User{Name: username, Score: score, Rank: rank + 1}
+	nUser := User{
+		PlayerID: 	playerID,
+		Score: 		score,
+		Rank: 		rank + 1,
+	}
 	return nUser, err
 }
 
 func (l *Leaderboard) TotalMembers() int {
 	conn := getConnection(l.Settings)
+	defer conn.Close()
 	total, err := redis.Int(conn.Do("ZCARD", l.Name))
 	if err != nil {
 		fmt.Printf("error on get leaderboard total members")
 		return 0
 	}
-	defer conn.Close()
 	return total
 }
 
-func (l *Leaderboard) RemoveMember(username string) (User, error) {
+func (l *Leaderboard) RemoveMember(playerID uint64) (User, error) {
 	conn := getConnection(l.Settings)
-	nUser, err := l.GetMember(username)
-	_, err = conn.Do("ZREM", l.Name, username)
+	defer conn.Close()
+	nUser := l.GetMember(playerID)
+	_, err := conn.Do("ZREM", l.Name, playerID)
 	if err != nil {
 		fmt.Printf("error on remove user from leaderboard")
 	}
-	defer conn.Close()
 	return nUser, err
 }
 
 func (l *Leaderboard) TotalPages() int {
 	conn := getConnection(l.Settings)
+	defer conn.Close()
 	pages := 0
 	total, err := redis.Int(conn.Do("ZCOUNT", l.Name, "-inf", "+inf"))
 	if err == nil {
 		pages = int(math.Ceil(float64(total) / float64(l.PageSize)))
 	}
-	defer conn.Close()
 	return pages
 }
 
-func (l *Leaderboard) GetMember(username string) (User, error) {
+func (l *Leaderboard) GetMember(playerID uint64) User {
 	conn := getConnection(l.Settings)
-	rank, err := redis.Int(conn.Do("ZREVRANK", l.Name, username))
+	defer conn.Close()
+	rank, err := redis.Int(conn.Do("ZREVRANK", l.Name, playerID))
 	if err != nil {
 		rank = 0
 	}
-	score, err := redis.Int(conn.Do("ZSCORE", l.Name, username))
+	score, err := redis.Int(conn.Do("ZSCORE", l.Name, playerID))
 	if err != nil {
 		score = 0
 	}
-	defer conn.Close()
-	nUser := User{Name: username, Score: score, Rank: rank + 1}
-	return nUser, err
+	nUser := User{
+		PlayerID: playerID,
+		Score: score,
+		Rank: rank + 1,
+	}
+	// If err is not nil, it will pose a problem for error checking
+	// since it was eaten above, therefore err was removed from being passed back.
+	return nUser
 }
 
-func (l *Leaderboard) GetAroundMe(username string) []User {
-	currentUser, _ := l.GetMember(username)
+func (l *Leaderboard) GetAroundMe(playerID uint64) []User {
+	currentUser := l.GetMember(playerID)
 	startOffset := currentUser.Rank - (l.PageSize / 2)
 	if startOffset < 0 {
 		startOffset = 0
@@ -170,9 +208,9 @@ func (l *Leaderboard) GetAroundMe(username string) []User {
 	return getMembersByRange(l.Settings, l.Name, l.PageSize, startOffset, endOffset)
 }
 
-func (l *Leaderboard) GetRank(username string) int {
+func (l *Leaderboard) GetRank(playerID uint64) int {
 	conn := getConnection(l.Settings)
-	rank, _ := redis.Int(conn.Do("ZREVRANK", l.Name, username))
+	rank, _ := redis.Int(conn.Do("ZREVRANK", l.Name, playerID))
 	defer conn.Close()
 	return rank + 1
 }
@@ -190,6 +228,7 @@ func (l *Leaderboard) GetLeaders(page int) []User {
 		startOffset = 0
 	}
 	endOffset := (startOffset + l.PageSize) - 1
+
 	return getMembersByRange(l.Settings, l.Name, l.PageSize, startOffset, endOffset)
 }
 
@@ -207,6 +246,18 @@ func (l *Leaderboard) GetMemberByRank(position int) User {
 	}
 	defer conn.Close()
 	return User{}
+}
+
+// Clears out all the databases
+func (l *Leaderboard) FlushDB() (err error) {
+	conn := getConnection(l.Settings)
+	defer conn.Close()
+
+	_, err = conn.Do("FLUSHALL")
+	if err != nil {
+		fmt.Printf("error on remove user from leaderboard")
+	}
+	return err
 }
 
 /* End Public functions */
